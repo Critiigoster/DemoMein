ae0ea27bce2e5868f1b68dfbff08a956d48c4506
Ihor Holovanov <begroom111@gmail.com>
2019-08-20 11:14:16 +0300

    FirstKindaProject

---
+++ Hello.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+class Program
+{
+
+
+
+    static void Main()
+    {
+
+
+        int a, b;
+        a = b = 5;
+        sum = a + b;
+        Console.WriteLine("Hello Mathematicion, your result => {0}", sum);
+        Console.WriteLine("Secons Version and factors equals {0} and {1} respectively", a, b);
+
+
+    }
+
+}
