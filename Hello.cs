f0ea52fb52ec0b594bfd4c283b37640238694328
Ihor Holovanov <begroom111@gmail.com>
2019-08-20 11:21:32 +0300

    SecondModifiedVersion

--- Hello.cs
+++ Hello.cs
@@ -10,13 +10,13 @@ class Program
     {
 
 
-        int a, b;
-        a = b = 5;
-        sum = a + b;
-        Console.WriteLine("Hello Mathematicion, your result => {0}", sum);
-        Console.WriteLine("Secons Version and factors equals {0} and {1} respectively", a, b);
+        int a, b; // vars(factors)
+        a = b = 5; // assign both factors woth the same value of 5
+        sum = a + b; // here we are product of two factors
+        Console.WriteLine("Hello Mathematicion, your result => {0}", sum); // ouput info about sum
+        Console.WriteLine("Secons Version and factors equals {0} and {1} respectively", a, b); // output information about factors
 
-
+        Console.ReadLine();// Add some features in order to differ our program!
     }
 
 }
